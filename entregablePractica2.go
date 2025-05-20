cf1f2f6892d62b76681eacbe914a244d881df17f
paganoalvi <alvapagano819@gmail.com>
2025-05-20 13:53:58 -0300

    Primera version del entregable con todos los metodos pedidos implementados(falta testear Insert)

--- entregablePractica2.go
+++ entregablePractica2.go
@@ -41,8 +41,8 @@ func New(s []int) OptimumSlice {
 	numAct := s[0]
 	repe := 1
 
-	for i := 1; i < len(s); i++ {
-		if s[i] == numAct {
+	for i := 1; i < len(s); i++ { // for tradicional de 1 hasta len(s)
+		if s[i] == numAct { //while
 			repe++
 		} else {
 			secuenciaNumeros = append(secuenciaNumeros, secuencia{valor: numAct, repeticiones: repe})
@@ -79,6 +79,71 @@ func isEmpty(os OptimumSlice) bool {
 	return (len(os.secuenciasNumeros) == 0)
 }
 
+// Devolvemos el primer "valor" del Os
+func FrontElemen(os OptimumSlice) int {
+	if isEmpty(os) {
+		panic("OptimumSlice vacio")
+	}
+	return os.secuenciasNumeros[0].valor
+}
+
+// Devolvemos el ultimo elemento del Os
+func LastElement(os OptimumSlice) int {
+	if isEmpty(os) {
+		panic("Optimum Slice vacio")
+	}
+	return os.secuenciasNumeros[len(os.secuenciasNumeros)-1].valor
+	//len(os.secuenciasNumeros) = 3 | -1 porque los indices comienzan en 0
+}
+
+// Implementamos Insert => Recibe un ptro a OS,un elemento(int),y una posicion(int) retorna un int
+func Insert(os *OptimumSlice, elem int, pos int) int {
+	if pos < 0 || pos > len(os.secuenciasNumeros) {
+		panic("Posicion invalida para insertar")
+	}
+	if isEmpty(*os) { //si esta vacio, inserto al principio
+		os.secuenciasNumeros = append(os.secuenciasNumeros, secuencia{elem, 1})
+	}
+	posLog := 0
+	for i, r := range os.secuenciasNumeros {
+		if posLog+r.repeticiones > pos {
+			// insertar en medio de secuenciaNumeros
+			offset := pos - posLog
+			if r.valor == elem {
+				// caso 1: mismo valor => aumentar repeticiones
+				os.secuenciasNumeros[i].repeticiones++
+				return i
+			}
+			// caso 2: valor distinto => partir el run en dos y meter el nuevo valor
+			antes := secuencia{r.valor, offset}
+			nuevaSecuen := secuencia{elem, 1}
+			despues := secuencia{r.valor, r.repeticiones - offset}
+			// remplazamos secuencia original con antes + nuevaSecuen + despues
+			nuevaSecuencias := []secuencia{}
+			nuevaSecuencias = append(nuevaSecuencias, os.secuenciasNumeros[:i]...)
+			if antes.repeticiones > 0 {
+				nuevaSecuencias = append(nuevaSecuencias, antes)
+			}
+			nuevaSecuencias = append(nuevaSecuencias, nuevaSecuen)
+			if despues.repeticiones > 0 {
+				nuevaSecuencias = append(nuevaSecuencias, despues)
+			}
+			nuevaSecuencias = append(nuevaSecuencias, os.secuenciasNumeros[i+1:]...)
+			os.secuenciasNumeros = nuevaSecuencias
+			return i
+		}
+		posLog += r.repeticiones
+	}
+	// ultimo caso: insertar al final
+	ult := os.secuenciasNumeros[len(os.secuenciasNumeros)-1]
+	if ult.valor == elem {
+		os.secuenciasNumeros[len(os.secuenciasNumeros)-1].repeticiones++
+	} else {
+		os.secuenciasNumeros = append(os.secuenciasNumeros, secuencia{elem, 1})
+	}
+	return len(os.secuenciasNumeros) - 1
+}
+
 func main() {
 	sl := []int{}
 	os := New(sl)
@@ -91,5 +156,12 @@ func main() {
 	fmt.Println(sliceAgain) // segunda prueba
 
 	fmt.Println(isEmpty(o))
+	Insert(&o, 4, 2)
+	fmt.Println(o)
+	s = SliceArray(o)
+	fmt.Println(s)
+
+	//fmt.Println(FrontElemen(o))
+	//fmt.Println(LastElement(os))
 
 }
